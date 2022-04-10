4e393a68a86af7749c33aa4c511ff1c5a32e619e
ranaapay <ranapay.itu@gmail.com>
2022-04-11 01:36:52 +0300

    category service added

---
+++ src/service/category.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"PicusFinalCase/src/models"
+	"PicusFinalCase/src/pkg/helper"
+	"PicusFinalCase/src/repository"
+	"io"
+)
+
+type CategoryService struct {
+	repo *repository.CategoryRepository
+}
+
+func NewCategoryService(repo *repository.CategoryRepository) *CategoryService {
+	return &CategoryService{repo: repo}
+}
+
+func (s *CategoryService) CreateCategories(file io.Reader) {
+	categories := helper.ReadCSVForCategory(file)
+	s.repo.CreateCategories(categories)
+}
+
+func (s *CategoryService) FindCategories() *[]models.Category {
+	return s.repo.FindCategories()
+}
