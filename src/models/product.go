6d0f2e3b00f37067eb719eb5fc6f6ab487c002fc
ranaapay <ranapay.itu@gmail.com>
2022-04-12 02:26:56 +0300

    getters and setters added

--- src/models/product.go
+++ src/models/product.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/shopspring/decimal"
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
 
 type Product struct {
 	Base
@@ -15,4 +18,24 @@ type Product struct {
 func (Product) TableName() string {
 	//default table name
 	return "products"
+}
+
+func (p *Product) GetProductName() string {
+	return p.ProductName
+}
+func (p *Product) GetProductPrice() decimal.Decimal {
+	return p.Price
+}
+func (p *Product) GetProductStockNumber() int {
+	return p.StockNumber
+}
+func (p *Product) GetProductCategoryId() string {
+	return p.CategoryId
+}
+
+func (p *Product) SetProductId(id string) {
+	p.Id = id
+}
+func (p *Product) SetProductUpdatedAt() {
+	p.UpdatedAt = time.Now()
 }
