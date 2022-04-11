9ef1444a7aee5abff08b89e18c49b904dc94ccc8
ranaapay <ranapay.itu@gmail.com>
2022-04-11 15:47:45 +0300

    product errors added

---
+++ src/pkg/errorHandler/productErrors.go
@@ -0,0 +1,29 @@
+package errorHandler
+
+import (
+	_type "PicusFinalCase/src/pkg/errorHandler/type"
+	"net/http"
+)
+
+var (
+	ProductNameValidationError = _type.ErrorType{
+		Code:    http.StatusBadRequest,
+		Message: "ProductName is required, it can not be empty.",
+	}
+	CategoryIdValidationError = _type.ErrorType{
+		Code:    http.StatusBadRequest,
+		Message: "CategoryId is required, it can not be empty.",
+	}
+	CategoryIdNotValidError = _type.ErrorType{
+		Code:    http.StatusBadRequest,
+		Message: "A category with the Category Id you entered could not be found. Please enter a valid id.",
+	}
+	StockNumberValidationError = _type.ErrorType{
+		Code:    http.StatusBadRequest,
+		Message: "StockNumber is required, it can not be lower than 1.",
+	}
+	PriceValidationError = _type.ErrorType{
+		Code:    http.StatusBadRequest,
+		Message: "Price is required, it can not be lower than 1.",
+	}
+)
