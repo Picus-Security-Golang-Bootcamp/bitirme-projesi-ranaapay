e9980c94b959ac9208857d240195593b13e49b05
ranaapay <ranapay.itu@gmail.com>
2022-04-11 23:46:08 +0300

    search filter logic added

---
+++ src/pkg/helper/setSearchFilter.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"PicusFinalCase/src/pkg/errorHandler"
+	"net/url"
+	"strconv"
+)
+
+var (
+	StockNumberVar = "stock_number"
+)
+
+func SetSearchFilter(values url.Values) map[string]interface{} {
+	var filter = make(map[string]interface{})
+	for i, v := range values {
+		filter[i] = v[0]
+		switch {
+		case i == StockNumberVar:
+			stockNumber, err := strconv.Atoi(filter[StockNumberVar].(string))
+			if err != nil {
+				errorHandler.Panic(errorHandler.ConvertError)
+			}
+			filter[i] = stockNumber
+		}
+	}
+	return filter
+}
