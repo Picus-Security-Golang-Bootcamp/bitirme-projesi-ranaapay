5a0ba00867211fc1eb068793b27e1da61a3db209
ranaapay <ranapay.itu@gmail.com>
2022-04-15 13:19:49 +0300

    order cancel error added

--- src/pkg/errorHandler/orderErrors.go
+++ src/pkg/errorHandler/orderErrors.go
@@ -14,4 +14,8 @@ var (
 		Code:    http.StatusBadRequest,
 		Message: "There are no products in your cart.",
 	}
+	OrderCanNotCancelledError = _type.ErrorType{
+		Code:    http.StatusBadRequest,
+		Message: "14 days have passed since the order was created, the cancellation failed.",
+	}
 )
