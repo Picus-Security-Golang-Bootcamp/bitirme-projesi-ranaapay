27b64fdfde9bc7434a8c6d09339947e3054b2626
ranaapay <ranapay.itu@gmail.com>
2022-04-15 03:35:28 +0300

    listOrders added.

--- src/handler/order.go
+++ src/handler/order.go
@@ -17,12 +17,17 @@ func NewOrderHandler(r *gin.RouterGroup, config config.JWTConfig, orderService *
 	h := &OrderHandler{service: orderService}
 
 	r.POST("", middleware.AuthMiddleware(config.SecretKey), h.completeOrder)
+	r.GET("", middleware.AuthMiddleware(config.SecretKey), h.listOrders)
 }
 
 func (h *OrderHandler) completeOrder(c *gin.Context) {
 	userId, _ := c.Get("id")
 	res := h.service.CreateOrder(userId.(string))
-
 	c.JSON(http.StatusCreated, responseType.NewResponseType(http.StatusCreated, res))
+}
 
+func (h *OrderHandler) listOrders(c *gin.Context) {
+	userId, _ := c.Get("id")
+	res := h.service.ListOrders(userId.(string))
+	c.JSON(http.StatusOK, responseType.NewResponseType(http.StatusOK, responseType.NewOrdersResponseType(res)))
 }
