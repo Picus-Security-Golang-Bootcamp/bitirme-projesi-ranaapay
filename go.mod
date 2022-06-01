a574ff1591bacaafae5c9efc20054b3ccf54587e
ranaapay <ranapay.itu@gmail.com>
2022-06-01 14:07:02 +0300

    cobra added project for all roots together

--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.11.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -52,6 +53,7 @@ require (
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
--- src/cmd/basket.go
+++ src/cmd/basket.go
@@ -12,8 +12,10 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 	swaggerfiles "github.com/swaggo/files" // swagger embed files
 	"github.com/swaggo/gin-swagger"        // gin-swagger middleware
+	"gorm.io/gorm"
 	"net/http"
 	"os"
 	"time"
@@ -23,22 +25,78 @@ var (
 	ConfigFile = "./pkg/config/config"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "basket",
+	Short: "basket",
+	Long:  `basic basket api`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := LoadConfig()
+
+		db := NewSqlDb(cfg)
+
+		SetLog()
+
+		gin.SetMode(gin.ReleaseMode)
+		r := gin.New()
+
+		SwaggerSettings(cfg.ServerConfig)
+		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+		srv := &http.Server{
+			Addr:         fmt.Sprintf("127.0.0.1:%s", cfg.ServerConfig.Port),
+			Handler:      r,
+			ReadTimeout:  time.Duration(cfg.ServerConfig.ReadTimeoutSecs * int64(time.Second)),
+			WriteTimeout: time.Duration(cfg.ServerConfig.WriteTimeoutSecs * int64(time.Second)),
+		}
+
+		rootRouter := r.Group(cfg.ServerConfig.RoutePrefix)
+		rootRouter.Use(middleware.Recovery())
+		rootRouter.Use(middleware.LoggingMiddleware())
+
+		authenticationRouter := rootRouter.Group("/authentication")
+		authRepo := repository.NewAuthRepository(db)
+		authService := service.NewAuthService(cfg.JWTConfig, authRepo)
+		handler.NewAuthHandler(authenticationRouter, authService)
+
+		categoryRouter := rootRouter.Group("/category")
+		categoryRepo := repository.NewCategoryRepository(db)
+		categoryService := service.NewCategoryService(&categoryRepo)
+		handler.NewCategoryHandler(categoryRouter, cfg.JWTConfig, categoryService)
+
+		productRouter := rootRouter.Group("/product")
+		productRepo := repository.NewProductRepository(db)
+		productService := service.NewProductService(productRepo, &categoryRepo)
+		handler.NewProductHandler(productRouter, cfg.JWTConfig, productService)
+
+		cartRouter := rootRouter.Group("/cart")
+		cartRepo := repository.NewCartRepository(db)
+		cartService := service.NewCartService(cartRepo, productRepo)
+		handler.NewCartHandler(cartRouter, cfg.JWTConfig, cartService)
+
+		orderRouter := rootRouter.Group("/order")
+		orderRepo := repository.NewOrderRepository(db)
+		orderService := service.NewOrderService(orderRepo, productRepo, cartRepo)
+		handler.NewOrderHandler(orderRouter, cfg.JWTConfig, orderService)
+
+		go func() {
+			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				log.Fatalf("listen: %s\n", err)
+			}
+		}()
+
+		graceful.ShutdownGin(srv, time.Duration(cfg.ServerConfig.TimeoutSecs*int64(time.Second)))
+
+	},
+}
+
+/*
 func Execute() {
 
-	//Read Config yaml
-	cfg, err := config.LoadConfig(ConfigFile)
-	if err != nil {
-		log.Fatalf("LoadConfig: %v", err)
-	}
+	cfg := LoadConfig()
 
-	//Initialize db
-	db, err := db.NewPsqlDB(cfg)
-	if err != nil {
-		log.Fatalf("DatabaseConnect: %v", err)
-	}
+	db := NewSqlDb(cfg)
 
-	log.SetOutput(os.Stdout)
-	log.SetFormatter(&log.JSONFormatter{})
+	SetLog()
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
@@ -83,12 +141,44 @@ func Execute() {
 	handler.NewOrderHandler(orderRouter, cfg.JWTConfig, orderService)
 
 	go func() {
-		if err = srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
 	graceful.ShutdownGin(srv, time.Duration(cfg.ServerConfig.TimeoutSecs*int64(time.Second)))
+}
+*/
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
+}
+
+// LoadConfig Read Config yaml
+func LoadConfig() *config.Config {
+	cfg, err := config.LoadConfig(ConfigFile)
+	if err != nil {
+		log.Fatalf("LoadConfig: %v", err)
+	}
+	return cfg
+}
+
+// NewSqlDb Initialize db
+func NewSqlDb(cfg *config.Config) *gorm.DB {
+	db, err := db.NewPsqlDB(cfg)
+	if err != nil {
+		log.Fatalf("DatabaseConnect: %v", err)
+	}
+	return db
+}
+
+// SetLog Setup logger
+func SetLog() {
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&log.JSONFormatter{})
 }
 
 func SwaggerSettings(cfg config.ServerConfig) {
